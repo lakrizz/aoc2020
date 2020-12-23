d7fb15b63ab6af3797aef6d351bc1414950c6ac6
Kristof <krizz@krizzblog.de>
2020-12-23 01:53:46 +0100

    adds day 4. part 2 was deffo not my favorite, less nifty thinking, more typing

--- 4/main.go
+++ 4/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
+	"strings"
 
 	"github.com/k0kubun/pp"
 )
@@ -14,12 +16,91 @@ func main() {
 		panic(err)
 	}
 
-	regex := *regexp.MustCompile(`^(?:.+[\n\r])+`)
-	fmt.Println(string(dat))
-	passports := regex.FindAllStringSubmatch(string(dat), -1)
-	pp.Println(passports)
+	ap := make([]map[string]string, 0)
+	passes := strings.Split(string(dat), "\n\n")
+	reg := *regexp.MustCompile("((.*?):(.*?))\\s")
 
-	for _, v := range passports {
-		pp.Println(v)
+	for _, v := range passes {
+		pass := strings.ReplaceAll(v, "\n", " ")
+		matches := reg.FindAllStringSubmatch(fmt.Sprintf("%s ", pass), -1)
+		m := make(map[string]string)
+		for _, k := range matches {
+			m[k[2]] = k[3]
+		}
+		ap = append(ap, m)
 	}
+
+	req_fields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"} // missing cid is fine
+	cvalid := 0
+	pp.Println(ap)
+	for _, v := range ap { // each map
+		valid := true
+		for _, r := range req_fields {
+			if _, ok := v[r]; ok {
+				valid = valid && ok
+			} else {
+				valid = false
+			}
+		}
+		if valid && isvalid(v) {
+			cvalid++
+		}
+	}
+	fmt.Println(cvalid)
+}
+
+func isvalid(input map[string]string) bool {
+	val := true
+	for k, v := range input {
+		switch k {
+		case "byr":
+			i, _ := strconv.Atoi(v)
+			val = val && bt(1920, 2002, i)
+			break
+
+		case "iyr":
+			i, _ := strconv.Atoi(v)
+			val = val && bt(2010, 2020, i)
+			break
+
+		case "eyr":
+			i, _ := strconv.Atoi(v)
+			val = val && bt(2020, 2030, i)
+			break
+
+		case "hgt":
+			if strings.Contains(v, "cm") {
+				i, _ := strconv.Atoi(v[:len(v)-2])
+				val = val && bt(150, 193, i)
+			} else if strings.Contains(v, "in") {
+				i, _ := strconv.Atoi(v[:len(v)-2])
+				val = val && bt(59, 76, i)
+			} else {
+				val = false
+			}
+			break
+
+		case "hcl":
+			reg := *regexp.MustCompile("#[0-9a-fA-F]{6}$")
+			val = val && reg.MatchString(v)
+			break
+
+		case "ecl":
+			reg := regexp.MustCompile("^(amb|blu|brn|gry|grn|hzl|oth)$")
+			val = val && reg.Match([]byte(v))
+			break
+
+		case "pid":
+			reg := *regexp.MustCompile("^[0-9]{9}$")
+			val = val && reg.MatchString(v)
+			break
+		}
+		fmt.Println(k, val)
+	}
+
+	return val
+}
+
+func bt(min, max, num int) bool {
+	return num >= min && num <= max
 }
